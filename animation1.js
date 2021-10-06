18f09d59f4e1e023b442568e0fbfac473591c5af
AnanyaAgarwal <ananya.agarwal622@gmail.com>
2021-10-06 13:46:30 +0530

    candle image added

--- animation1.js
+++ animation1.js
@@ -11,3 +11,16 @@ function move()
     bal.style.marginTop=pos+'px';
     bal.style.marginRight=pos+'px';
 }
+var can=setInterval(fly,15)
+poe=0;
+function fly()
+{
+    if(poe==-400)
+    {
+        poe=0;
+    }
+    poe--;
+    var can=document.getElementById("image2");
+    can.style.marginTop=poe+'px';
+    can.style.marginRight=poe+'px';
+}
